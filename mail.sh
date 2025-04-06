a0aa8b50e4590183fb92557c5e5b4b876d2d2e80
DevOpsProCloud <git-repo@devopsprocloud.in>
2025-04-06 18:17:05 +0530

    practice

---
+++ mail.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+TO_TEAM=$1
+SUBJECT=$2
+ALERT_TYPE=$3
+BODY=$4
+TO_ADDRESS=$5
+
+ESCAPE_BODY=$(printf '%s\n' "$BODY" | sed -e 's/[]\/$*.^[]/\\&/g')
+
+FINAL_BODY=$(sed -e "s/TO_TEAM/$TO_TEAM/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/BODY/$ESCAPE_BODY/g" template.html)
+
+echo "$FINAL_BODY" | mail -s $(echo -e "$SUBJECT\nContent-Type: text/html")" "$TO_ADDRESS"
+
