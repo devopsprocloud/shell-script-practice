8f0e49d359cbbdbafd30fc288fbf20dcf7f85a2a
DevOpsProCloud <git-repo@devopsprocloud.in>
2025-04-06 17:51:39 +0530

    disk-usage-practice

---
+++ 01-disk-usage.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+disk_usage=$(df -ht | grep 'xvd')
+threshold=1
+message=""
+
+while IFS= read -r line
+do
+    current_usage=$(echo $line | awk {'print $6F'} | cut -d % -f1)
+    disk_partition=$(echo $line | awk {'print $1F'})
+    if [ $current_usage -gt $threshold ]
+    then            
+        message+="High Disk Usage on $disk_partition: $current_usage %"
+done <<< $disk_usage
+
