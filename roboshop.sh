473503f95d1d41c758f07d5d8a47b67abf064044
DevOpsProCloud <git-repo@devopsprocloud.in>
2025-04-07 10:26:42 +0530

    shell0script-practice

--- roboshop.sh
+++ roboshop.sh
@@ -8,14 +8,16 @@ INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipp
 for i in "${INSTANCES[@]}"
 do
     if
-        [ $i == "mongodb" ] || [ $i== "mysql" ] || [ $i == "shipping" ]
+        [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
     then    
         INSTANCE_TYPE=t3.small
     else
         INSTANCE_TYPE=t2.micro  
     fi
-    aws2 ec2 run-instances --image-id $AMI_ID --instance-type $INSTANCE_TYPE --security-group-ids $SG_ID --region us-east-1 --tag-specifications "ResourceType=instance,Tags={Key=Name,Value=$i}"
+    IP_ADDRESS=$(aws2 ec2 run-instances --image-id $AMI_ID --instance-type $INSTANCE_TYPE --security-group-ids $SG_ID --region us-east-1 --tag-specifications "ResourceType=instance,Tags={Key=Name,Value=$i}" --query 'Insances[0].PrivateIpAddress' --output text --query 'Insances[0].PublicIpAddress' --output text)
+    echo "$i : $IP_ADDRESS"
 done
+
 
 #Trying
 
