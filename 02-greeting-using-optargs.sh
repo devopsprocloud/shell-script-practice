115061733b64319d7fff71ed2e5b35ca508c3c93
DevOpsProCloud <git-repo@devopsprocloud.in>
2025-04-07 08:31:51 +0530

    shell-script-practice

--- 02-greeting-using-optargs.sh
+++ 02-greeting-using-optargs.sh
@@ -7,7 +7,7 @@ USAGE (){
     echo "USAGE:: $(basename $0) -n <name> -w <wishes>"
     echo "OPTIONS::"
     echo "  -n, please specify the name (mandotory)"
-    echo "  -w, please specify the wishes (optional), defaults is: Good Morning"
+    echo "  -w, please specify the wishes (optional), Default value is: Good Morning"
     echo "  -h, Display Help and Exit"
 }
 
