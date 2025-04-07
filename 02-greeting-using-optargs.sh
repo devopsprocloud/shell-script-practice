971ea7bb0724d3b6c1589e42d8361adfd97849e3
DevOpsProCloud <git-repo@devopsprocloud.in>
2025-04-07 08:17:33 +0530

    shell-script-practice

--- 02-greeting-using-optargs.sh
+++ 02-greeting-using-optargs.sh
@@ -5,7 +5,12 @@ WISHES=""
 
 USAGE (){
     echo "Command:: $(basename $0) -n <name> -w <wishes>"
+    echo "USAGE::"
+    echo "  -n, please specify the name"
+    echo "  -w, please specify the wishes. Ex: Good Morning"
+    echo "  -h, Display Help and Exit"
 }
+
 while getopts ":n:w:h" opt; 
 do
     case $opt in 
@@ -16,5 +21,10 @@ do
     esac
 done
 
-echo "Hello $NAME. $WISHES."
+if [ -z $NAME ] || [ -z $WISHES ]
+then
+    echo "Both options -n and -w is mandatory"
+else
+    echo "Hello $NAME. $WISHES."
+fi
 
